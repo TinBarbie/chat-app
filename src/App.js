52d84e7857bdd1254dc7a09a8ab0338d0d682f45
Nguyễn Huỳnh Bảo Tín <baotin22071998@gmail.com>
2023-12-21 17:11:55 +0700

    setup frontend reactjs and tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import io from "socket.io-client"
+
+const socket = io.connect("http://localhost:3001")
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-w-screen min-h-screen bg-gradient-to-b from-cyan-200 to-cyan-500">
+      <div className="flex flex-col items-center">
+        <h1 className="text-red-400 text-[52px] pb-10">
+          Messenger App
+        </h1>
+        <h3 className="text-red-600 text-[32px]">
+          List of chat rooms:
+        </h3>
+      </div>
     </div>
   );
 }
